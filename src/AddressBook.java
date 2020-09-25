67a32e29d18765e51b16767e5b75ef477873ebc2
Tejash Patel <tejashpatel3402@gmail.com>
2020-09-25 08:08:17 -0400

    second commit

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -3,6 +3,10 @@ import java.util.ArrayList;
 public class AddressBook {
     private ArrayList<BuddyInfo> address;
 
+    public AddressBook(){
+        address = new ArrayList<BuddyInfo>();
+    }
+
     public void addBuddy(BuddyInfo buddy){
         address.add(buddy);
     }
@@ -12,6 +16,9 @@ public class AddressBook {
     }
 
     public static void main(String[] args){
-        System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
 }
