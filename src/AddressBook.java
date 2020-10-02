b51d44fda2a38976273dbc6eb06e1e8191bb515e
Tejash Patel <tejashpatel3402@gmail.com>
2020-10-02 17:23:24 -0400

    add a new method

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -20,6 +20,10 @@ public class AddressBook {
         return null;
     }
 
+    public void testMethod() {
+        System.out.println("This method don't do anything.");
+    }
+
     public static void main(String[] args) {
         BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
         AddressBook addressBook = new AddressBook();
